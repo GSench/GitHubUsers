4882ec64066dce6f84473ea8c31b69d4b407b5e6
GSench <grishasenchenok@yandex.ru>
2017-05-01 14:39:34 +0300

    Android app for searching users on GitHub

---
+++ app/src/main/java/ru/gsench/githubusers/presentation/MainActivity.java
@@ -0,0 +1,15 @@
+package ru.gsench.githubusers.presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ru.gsench.githubusers.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
