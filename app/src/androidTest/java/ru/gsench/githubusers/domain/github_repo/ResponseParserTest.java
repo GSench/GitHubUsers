762988722242841a0fc124d467c8f2c13f310cb2
GSench <grishasenchenok@yandex.ru>
2017-05-04 21:57:12 +0300

    Response parser unit tests initial implementation

---
+++ app/src/androidTest/java/ru/gsench/githubusers/domain/github_repo/ResponseParserTest.java
@@ -0,0 +1,71 @@
+package ru.gsench.githubusers.domain.github_repo;
+
+import org.junit.Test;
+
+/**
+ * Created by Григорий Сенченок on 04.05.2017.
+ */
+
+public class ResponseParserTest {
+
+    @Test
+    public void parseSearchResultsTestCaseNorm(){
+
+    }
+
+    @Test
+    public void initUserShortTestCaseNorm(){
+
+    }
+
+    @Test
+    public void parseRepositoriesTestCaseNorm(){
+
+    }
+
+    @Test
+    public void parseUserTestCaseNorm(){
+
+    }
+
+    @Test
+    public void parseSearchResultsTestCaseNull(){
+
+    }
+
+    @Test
+    public void initUserShortTestCaseNull(){
+
+    }
+
+    @Test
+    public void parseRepositoriesTestCaseNull(){
+
+    }
+
+    @Test
+    public void parseUserTestCaseNull(){
+
+    }
+
+    @Test
+    public void parseSearchResultsTestCaseWrong(){
+
+    }
+
+    @Test
+    public void initUserShortTestCaseWrong(){
+
+    }
+
+    @Test
+    public void parseRepositoriesTestCaseWrong(){
+
+    }
+
+    @Test
+    public void parseUserTestCaseWrong(){
+
+    }
+
+}
